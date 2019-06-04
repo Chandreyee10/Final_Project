d193bb70161e6bb0db72aa871249cdd3788b9ca1
Chandreyee10 <48053771+Chandreyee10@users.noreply.github.com>
2019-06-04 15:25:51 +0530

    Update AuthorizationTest.java

--- src/test/java/bobcat/test/login/AuthorizationTest.java
+++ src/test/java/bobcat/test/login/AuthorizationTest.java
@@ -51,7 +51,7 @@ public class AuthorizationTest {
 		catch(Exception e) {
 			
 		}
-		boolean flag=signinpage.open().login("admin","admin");
+		boolean flag=signinpage.open().login("admin1","admin1");
 		//signinpage.open().login("admin","admin");
 		if (flag==true)
 		{
